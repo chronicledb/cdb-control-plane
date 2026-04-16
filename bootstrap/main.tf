9edb95e16310c644f7530e505c2601d033e18ee1
Grant Chen <grantchen2003@gmail.com>
2026-04-15 21:14:32 -0400

    feat(bootstrap): add ECR repositories for tx-managers and appliers
    - Add aws_ecr_repository resources for cdb-tx-managers and cdb-appliers
    - Enable scan_on_push and force_delete for both repositories
    - Add Terraform outputs for the new repository URLs

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -37,6 +37,20 @@ resource "aws_ecr_repository" "cdb_control_plane" {
   }
 }
 
+resource "aws_ecr_repository" "cdb_tx_managers" {
+  name                 = "cdb-tx-managers"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "cdb-tx-managers"
+  }
+}
+
 resource "aws_ecr_repository" "cdb_storage_engines" {
   name                 = "cdb-storage-engines"
   image_tag_mutability = "MUTABLE"
@@ -51,6 +65,20 @@ resource "aws_ecr_repository" "cdb_storage_engines" {
   }
 }
 
+resource "aws_ecr_repository" "cdb_appliers" {
+  name                 = "cdb-appliers"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "cdb-appliers"
+  }
+}
+
 # ---------------------------------------------------------------------------
 # Outputs
 # ---------------------------------------------------------------------------
@@ -59,6 +87,14 @@ output "cdb_control_plane_ecr_url" {
   value = aws_ecr_repository.cdb_control_plane.repository_url
 }
 
+output "cdb_tx_managers_ecr_url" {
+  value = aws_ecr_repository.cdb_tx_managers.repository_url
+}
+
 output "cdb_storage_engines_ecr_url" {
   value = aws_ecr_repository.cdb_storage_engines.repository_url
+}
+
+output "cdb_appliers_ecr_url" {
+  value = aws_ecr_repository.cdb_appliers.repository_url
 }
